9f38737f85719dc6e85a6f5c3e9992c24abdb868
Christina Zammit <zammichr@sheridancollege.ca>
2021-11-27 23:03:19 -0500

    Updated the book class to include the id, title, author, genre, and page number

--- API/Models/Entities/Book.cs
+++ API/Models/Entities/Book.cs
@@ -7,6 +7,10 @@ namespace API.Models.Entities
 {
     public class Book
     {
-        
+        public Guid Id {get; set;}
+        public string Title {get; set;}
+        public List<Author> Author {get; set;}
+        public Genre Genre {get; set;}
+        public int PageNumber {get; set;}
     }
 }
