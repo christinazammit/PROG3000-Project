266ebdd721ec1b3403a4abef73043b1eff9b8428
Christina Zammit <zammichr@sheridancollege.ca>
2021-11-27 22:59:06 -0500

    Created the book class and updated the data context class

--- API/Models/Persistence/DataContext.cs
+++ API/Models/Persistence/DataContext.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.EntityFrameworkCore;
+using API.Models.Entities;
 
 namespace API.Models.Persistence
 {
     public class DataContext : DbContext
     {
-
+        public DbSet<Book> Books {get; set;}
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("FileName=book.db");
