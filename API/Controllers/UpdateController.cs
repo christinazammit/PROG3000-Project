3502230230e8c5dcfeb3f8efa1db4d788ffb1788
Christina Zammit <zammichr@sheridancollege.ca>
2021-12-10 14:26:13 -0500

    Added a method to the update controller that allows users to update a book

--- API/Controllers/UpdateController.cs
+++ API/Controllers/UpdateController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 using API.Models.Persistence;
+using API.Models.Entities;
 
 namespace API.Controllers
 {
@@ -17,6 +19,36 @@ namespace API.Controllers
         public UpdateController(DataContext context)
         {
             _context = context;
+        }
+
+        [HttpPut("{id}")] // api/update/id
+        public async Task<IActionResult> UpdateBook(string id, Book book)
+        {
+            try
+            {
+                var bookInDb = await _context.Books.Include(x => x.Author).FirstOrDefaultAsync(x => x.Id.Equals(new Guid(id)));
+
+                if (bookInDb == null) 
+                {
+                    return NotFound();
+                }
+
+                bookInDb.Title = book.Title;
+                bookInDb.Author = new Author {
+                    FirstName = book.Author.FirstName,
+                    LastName = book.Author.LastName,
+                };
+                bookInDb.Genre = book.Genre;
+                bookInDb.Comments = book.Comments;
+
+                await _context.SaveChangesAsync();
+                return Ok(bookInDb);
+
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
         }
     }
 }
