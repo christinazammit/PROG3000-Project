e1f4d110422bce94204a8f7986c79c2841b2ad62
Christina Zammit <zammichr@sheridancollege.ca>
2021-12-10 14:17:00 -0500

    Updated the db context class and added a new migration

--- API/Models/Persistence/DataContext.cs
+++ API/Models/Persistence/DataContext.cs
@@ -11,6 +11,7 @@ namespace API.Models.Persistence
     public class DataContext : DbContext
     {
         public DbSet<Book> Books {get; set;}
+        public DbSet<Author> Authors {get; set;}
         
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
