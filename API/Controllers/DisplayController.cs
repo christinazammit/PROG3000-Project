bf0b952c525c13e18f703dc9978310622295ba9e
Christina Zammit <zammichr@sheridancollege.ca>
2021-12-10 14:21:26 -0500

    Added a method in the display class to retrieve all books in the database

--- API/Controllers/DisplayController.cs
+++ API/Controllers/DisplayController.cs
@@ -33,5 +33,31 @@ namespace API.Controllers
             return View();
         } 
 
+        [HttpGet("books")] // api/display/books
+        public IActionResult GetAllBooks()
+        {
+            List<Book> books = new List<Book>();
+
+            foreach (var book in _context.Books.Include(x => x.Author))
+            {
+                var gotBook = new Book {
+                    Id = book.Id,
+                    Title = book.Title,
+                    Author = new Author {
+                        FirstName = book.Author.FirstName,
+                        LastName = book.Author.LastName,
+                    },
+                    Genre = book.Genre,
+                    CurrentPage = book.CurrentPage,
+                    TotalPages = book.TotalPages,
+                    IsBookComplete = book.IsBookComplete,
+                    Comments = book.Comments,
+                };
+
+                books.Add(gotBook);
+            }
+
+            return Ok(books);
+        }
     }
 }
