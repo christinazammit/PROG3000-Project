b14718e5e1a5246e946c0dfede633bfe4853b344
Christina Zammit <zammichr@sheridancollege.ca>
2021-12-10 14:49:10 -0500

    Added a new migration and updated the book table

---
+++ API/Migrations/20211210194822_UpdatedBookTable.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class UpdatedBookTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Authors_Books_BookId",
+                table: "Authors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Authors_BookId",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "BookId",
+                table: "Authors");
+
+            migrationBuilder.RenameColumn(
+                name: "PageNumber",
+                table: "Books",
+                newName: "TotalPages");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AuthorId",
+                table: "Books",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Comments",
+                table: "Books",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CurrentPage",
+                table: "Books",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBookComplete",
+                table: "Books",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Comments",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentPage",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "IsBookComplete",
+                table: "Books");
+
+            migrationBuilder.RenameColumn(
+                name: "TotalPages",
+                table: "Books",
+                newName: "PageNumber");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "BookId",
+                table: "Authors",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_BookId",
+                table: "Authors",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Authors_Books_BookId",
+                table: "Authors",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
