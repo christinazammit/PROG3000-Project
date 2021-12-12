c7eac47ce6ad41180a30ee71434e9542f433eacc
Christina Zammit <zammichr@sheridancollege.ca>
2021-12-11 23:42:05 -0500

    User can now update the page number or mark book as completed

--- API/Views/javascript/bookProgress.js
+++ API/Views/javascript/bookProgress.js
@@ -4,21 +4,29 @@ document.getElementById("allbooks-button").addEventListener("click", (element) =
 
 const id = localStorage.getItem("id");
 
-const updateProgress = {
-    currentpage: document.getElementById("currentPage").value
-};
-
 document.getElementById("updateButton").addEventListener("click", (element) => {
+    var updatedPage = document.getElementById("currentPage");
+    var updatedComplete;
+
+    if (document.getElementById("complete").checked) {
+        updatedComplete = true;
+    }
+    else {
+        updatedComplete = false;
+    }
+
     fetch(`http://localhost:5000/api/update/${id}/currentpage`, {
     method: "PUT",
     headers: {
         "Content-Type": "application/json"
     },
-    body: JSON.stringify(updateProgress)
+    body: JSON.stringify({
+        currentpage: updatedPage.value,
+        isbookcomplete: updatedComplete,
+    })
 })
 
     .then(response => response.json())
     .then(data => console.log(data))
-    //location.href = "index.html";
-    //console.log(updatedBook.title);
+    location.href = "index.html";
 });
