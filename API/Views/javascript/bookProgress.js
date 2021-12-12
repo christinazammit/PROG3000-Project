10abf0116c86a4b64a3afe8c3d9f0b5b9d694a53
Christina Zammit <zammichr@sheridancollege.ca>
2021-12-11 23:15:35 -0500

    Updated the modify book js file so user can now update title, author, genre, and comment of book

--- API/Views/javascript/bookProgress.js
+++ API/Views/javascript/bookProgress.js
@@ -3,3 +3,22 @@ document.getElementById("allbooks-button").addEventListener("click", (element) =
 });
 
 const id = localStorage.getItem("id");
+
+const updateProgress = {
+    currentpage: document.getElementById("currentPage").value
+};
+
+document.getElementById("updateButton").addEventListener("click", (element) => {
+    fetch(`http://localhost:5000/api/update/${id}/currentpage`, {
+    method: "PUT",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(updateProgress)
+})
+
+    .then(response => response.json())
+    .then(data => console.log(data))
+    //location.href = "index.html";
+    //console.log(updatedBook.title);
+});
--- API/Views/javascript/modifyBook.js
+++ API/Views/javascript/modifyBook.js
@@ -3,3 +3,51 @@ document.getElementById("allbooks-button").addEventListener("click", (element) =
 });
 
 const id = localStorage.getItem("id");
+
+document.getElementById("modifyButton").addEventListener("click", (element) => {
+    var updatedTitle = document.getElementById("title");
+    var updatedFirstName = document.getElementById("firstName");
+    var updatedLastName = document.getElementById("lastName");
+    var updatedGenre;
+
+    if (document.getElementById("romance").checked) {
+        updatedGenre = document.getElementById("romance");
+    }
+    else if (document.getElementById("mystery").checked) {
+        updatedGenre = document.getElementById("mystery");
+    }
+    else if (document.getElementById("historical").checked) {
+        updatedGenre = document.getElementById("historical");
+    }
+    else if (document.getElementById("nonfiction").checked) {
+        updatedGenre = document.getElementById("nonfiction");
+    }
+    else if (document.getElementById("fantasy").checked) {
+        updatedGenre = document.getElementById("fantasy");
+    }
+    else if (document.getElementById("horror").checked) {
+        updatedGenre = document.getElementById("horror");
+    }
+
+    var updatedComment = document.getElementById("comment");
+
+    fetch(`http://localhost:5000/api/update/${id}`, {
+    method: "PUT",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        title: updatedTitle.value,
+        author: {
+            firstName: updatedFirstName.value,
+            lastName: updatedLastName.value,
+        },
+        genre: updatedGenre.value,
+        comments: updatedComment.value,
+    })
+})
+
+    .then(response => response.json())
+    .then(data => console.log(data))
+    location.href = "index.html";
+})
